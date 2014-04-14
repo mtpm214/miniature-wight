89748379acbcf7288b54b7df9c255f284e60c7a3
CS214 <mcneil.paul@yahoo.com>
2014-04-14 18:43:57 -0400

    mauricio sucks dick

---
+++ customer.h
@@ -0,0 +1,18 @@
+#ifndef SETUP_H
+#define SETUP_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+typedef struct CustomerStruct{
+	char * name;
+	char * address;
+	float balance;
+}customer;
+
+CategoryPtr categoryCreate(char *);
+
+CustomerPtr customerCreate()
+
+#endif
